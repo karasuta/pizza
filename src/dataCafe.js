ce774dbef714cb4dbc38bb0d3a13c1fedf66e6e4
karasuta <kargina.ts@gmail.com>
2022-11-06 12:18:01 +0300

    first commit

---
+++ src/dataCafe.js
@@ -0,0 +1,26 @@
+export const dataCafe=[
+    {
+        id: 1,
+        image:'https://img.freepik.com/free-photo/three-firends-together-eating-pizza-in-a-cafe_1303-26152.jpg?size=626&ext=jpg&uid=R61916015&ga=GA1.2.589884259.1665691129&semt=sph'
+    },
+    {
+        id: 2,
+        image:'https://img.freepik.com/free-photo/young-pretty-woman-eating-pizza-at-pizza-bar_1303-20328.jpg?size=626&ext=jpg&uid=R61916015&ga=GA1.2.589884259.1665691129&semt=sph'
+    },
+    {
+        id: 3,
+        image:'https://img.freepik.com/free-photo/three-firends-together-eating-pizza-in-a-cafe_1303-26150.jpg?size=626&ext=jpg&uid=R61916015&ga=GA1.2.589884259.1665691129&semt=sph'
+    },
+    {
+        id: 4,
+        image:'https://img.freepik.com/free-photo/happy-little-girl-looks-at-pizza-with-appetite_169016-18646.jpg?size=626&ext=jpg&uid=R61916015&ga=GA1.2.589884259.1665691129&semt=sph'
+    },
+    {
+        id: 5,
+        image:'https://img.freepik.com/premium-photo/cozy-restaurant-with-people-and-waiter_175935-230.jpg?size=626&ext=jpg&uid=R61916015&ga=GA1.2.589884259.1665691129&semt=sph'
+    },
+    {
+        id: 6,
+        image:'https://img.freepik.com/free-photo/tables-and-chairs-arranged-in-empty-coffee-shop_107420-96463.jpg?size=626&ext=jpg&uid=R61916015&ga=GA1.2.589884259.1665691129&semt=sph'
+    }
+]
